318c365407f46d1d2e6a420b09fe8a006181b331
Aniketh Reddy <sunnyrockzzz1811@gmail.com>
2025-08-01 11:39:02 +0530

    Minor Improvements

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import CourseSelect from "./components/CourseSelect";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState({0:[36,40]})
+  const [data, setData] = useState({0:[36,4]})
   const [gpa, setGPA] = useState(9)
+  const [courseComponents, setCourseComponents] = useState([<CourseSelect index={0} key={0} setData={setData}/>]);
 
   useEffect(()=>{
     const result = Object.values(data).reduce((acc, curr) => acc + curr[0], 0)
     const total = Object.values(data).reduce((acc, curr) => acc + curr[1], 0)
+    console.log(result);
+    console.log(total);
     setGPA(result/total)
   }, [data])
-
-  const [courseComponents, setCourseComponents] = useState([<CourseSelect index={0} key={0} setData={setData}/>]);
 
   function handleClickAddCourseButton(){
     setCourseComponents((prev)=>[...prev, <CourseSelect index={prev.length} key={prev.length} setData={setData}/>])
@@ -24,7 +25,7 @@ function App() {
       {courseComponents}
     </div>
       <button onClick={handleClickAddCourseButton}>Add Course</button>
-      <h2>GPA: {gpa*10}</h2>
+      <h2>GPA: {gpa.toFixed(2)}</h2>
     </>
   );
 }
--- src/components/CourseSelect.jsx
+++ src/components/CourseSelect.jsx
@@ -1,30 +1,30 @@
 import { useState, useEffect } from "react";
 
-function CourseSelect({index, setData}) {
-    const [grade, setGrade] = useState(9);
-    const [credits, setCredits] = useState(4);
+function CourseSelect({ index, setData }) {
+	const [grade, setGrade] = useState(9);
+	const [credits, setCredits] = useState(4);
 
-    useEffect(() => {
-        setData((prev)=>{
-            return {...prev, [index]: [credits*grade, credits*10]}
-        })
-    }, [grade, credits])
+	useEffect(() => {
+		setData((prev) => {
+			return { ...prev, [index]: [credits * grade, credits] }
+		})
+	}, [grade, credits])
 
-    return (<div className="courseSelect">
-        <select defaultValue={9} onChange={(e) => setGrade(Number(e.target.value))}>
-            <option value={10}>S</option>
-            <option value={9}>A</option>
-            <option value={8}>B</option>
-            <option value={7}>C</option>
-            <option value={6}>D</option>
-            <option value={5}>E</option>
-        </select>
+	return (<div className="courseSelect">
+		<select defaultValue={9} onChange={(e) => setGrade(Number(e.target.value))}>
+			<option value={10}>S</option>
+			<option value={9}>A</option>
+			<option value={8}>B</option>
+			<option value={7}>C</option>
+			<option value={6}>D</option>
+			<option value={5}>E</option>
+		</select>
 
-        <select defaultValue={4} onChange={(e) => setCredits(Number(e.target.value))}>
-            <option value={4}>4</option>
-            <option value={3}>3</option>
-            <option value={2}>2</option>
-        </select></div>);
+		<select defaultValue={4} onChange={(e) => setCredits(Number(e.target.value))}>
+			<option value={4}>4</option>
+			<option value={3}>3</option>
+			<option value={2}>2</option>
+		</select></div>);
 }
 
 export default CourseSelect;
