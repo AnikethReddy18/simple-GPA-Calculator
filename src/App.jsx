21c3ca0cbd1076e521321a8296190b68a9b55769
Aniketh Reddy <sunnyrockzzz1811@gmail.com>
2025-08-01 12:08:14 +0530

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import CourseSelect from "./components/CourseSelect";
-import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 function App() {
-  const [data, setData] = useState({0:[36,4]})
-  const [gpa, setGPA] = useState(9)
-  const [courseComponents, setCourseComponents] = useState([<CourseSelect index={0} key={0} setData={setData}/>]);
-
-  useEffect(()=>{
-    const result = Object.values(data).reduce((acc, curr) => acc + curr[0], 0)
-    const total = Object.values(data).reduce((acc, curr) => acc + curr[1], 0)
-    console.log(result);
-    console.log(total);
-    setGPA(result/total)
-  }, [data])
-
-  function handleClickAddCourseButton(){
-    setCourseComponents((prev)=>[...prev, <CourseSelect index={prev.length} key={prev.length} setData={setData}/>])
-  }
-
   return (
     <>
-    <h1>VIT GPA Calculator</h1>
-    <div className="courseComponents">
-      {courseComponents}
-    </div>
-      <button onClick={handleClickAddCourseButton}>Add Course</button>
-      <h2>GPA: {gpa.toFixed(2)}</h2>
+    <h1>Welcome to VIT-AP Utilities</h1>
+    <Link to="gpa-calculator">GPA Calculator</Link>
     </>
   );
 }
 
 export default App;
+
 
---
+++ src/pages/gpaCalculator/GPACalculator.jsx
@@ -0,0 +1,31 @@
+import CourseSelect from "./components/CourseSelect";
+import { useEffect, useState } from "react";
+
+function GPACalculator() {
+  const [data, setData] = useState({0:[36,4]})
+  const [gpa, setGPA] = useState(9)
+  const [courseComponents, setCourseComponents] = useState([<CourseSelect index={0} key={0} setData={setData}/>]);
+
+  useEffect(()=>{
+    const result = Object.values(data).reduce((acc, curr) => acc + curr[0], 0)
+    const total = Object.values(data).reduce((acc, curr) => acc + curr[1], 0)
+    setGPA(result/total)
+  }, [data])
+
+  function handleClickAddCourseButton(){
+    setCourseComponents((prev)=>[...prev, <CourseSelect index={prev.length} key={prev.length} setData={setData}/>])
+  }
+
+  return (
+    <>
+    <h1>VIT GPA Calculator</h1>
+    <div className="courseComponents">
+      {courseComponents}
+    </div>
+      <button onClick={handleClickAddCourseButton}>Add Course</button>
+      <h2>GPA: {gpa.toFixed(2)}</h2>
+    </>
+  );
+}
+
+export default GPACalculator;
